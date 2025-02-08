db09613d5ad6b6714e9795f711fffedeccd24d23
norvisyunior <norviscabrera7@gmail.com>
2025-02-07 19:08:21 -0500

    Añadido script de seed para crear usuario administrador

--- userSeed.js
+++ userSeed.js
@@ -1,21 +1,23 @@
-import User from "./models/User.js"
-import bcrypt from "bcrypt"
-import connectToDataBase from "./db/db.js"
+import User from "./models/User.js";
+import bcrypt from "bcrypt";
+import connectToDataBase from "./db/db.js";
 
 const userRegister = async () => {
-    connectToDataBase()
-    try {
-        const hashPassword = await bcrypt.hash("admin", 10)
-        const newUser = new User({
-            name: "Norvis",
-            email: "norvis@gmail.com",
-            password: hashPassword,
-            role: "admin"
-        })
-        await newUser.save()
-    } catch (error) {
-        console.log(error)
-    }
-}
+  await connectToDataBase();
+  console.log("Conectado a la base de datos");
+  try {
+    const hashPassword = await bcrypt.hash("admin", 10);
+    const newUser = new User({
+      name: "Norvis",
+      email: "norvis@gmail.com",
+      password: hashPassword,
+      role: "admin"
+    });
+    await newUser.save();
+    console.log("Usuario administrador creado");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 userRegister();
